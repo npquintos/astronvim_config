ded1c3f887fb3c66392c3d8c50d258104b8f0c49
Noel Quintos <npquintos@gmail.com>
2024-04-22 10:43:58 -0600

    Update community.lua

--- lua/community.lua
+++ lua/community.lua
@@ -13,4 +13,5 @@ return {
   { import = "astrocommunity.file-explorer.oil-nvim" },
   { import = "astrocommunity.colorscheme.bamboo-nvim" },
   { import = "astrocommunity.note-taking.neorg" },
+  { import = "astrocommunity.snippet.nvim-snippets" },
 }
