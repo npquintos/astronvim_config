d8cd713b8be44c0d564dbec19a16854bcee39fc6
Noel Quintos <npquintos@gmail.com>
2024-05-08 10:38:58 -0600

    Add zooming in/out of pane

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -52,27 +52,39 @@ return {
         -- second key is the lefthand side of the map
 
         -- navigate buffer tabs with `H` and `L`
-        L = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        H = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+        N = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+        -- H = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
 
         -- mappings seen under group name "Buffer"
-        ["<Leader>bD"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
-          end,
-          desc = "Pick to close",
-        },
+        -- ["<Leader>bD"] = {
+        --   function()
+        --     require("astroui.status.heirline").buffer_picker(
+        --       function(bufnr) require("astrocore.buffer").close(bufnr) end
+        --     )
+        --   end,
+        --   desc = "Pick to close",
+        -- },
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
         ["<Leader>b"] = { desc = "Buffers" },
         -- quick save
         -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-        ["<right>"] = { ":let linenum=getpos('.')[1]|:wincmd l|:call cursor(linenum,0)<cr>", desc = "go to same line number at right pane" },
-        ["<left>"] = { ":let linenum=getpos('.')[1]|:wincmd h|:call cursor(linenum,0)<cr>", desc = "go to same line number at left pane" },
-        ["<up>"] = { ":let linenum=getpos('.')[1]|:wincmd k|:call cursor(linenum,0)<cr>", desc = "go to same line number at upper pane" },
-        ["<down>"] = { ":let linenum=getpos('.')[1]|:wincmd j|:call cursor(linenum,0)<cr>", desc = "go to same line number at lower pane" },
+        ["<right>"] = {
+          ":let linenum=getpos('.')[1]|:wincmd l|:call cursor(linenum,0)<cr>",
+          desc = "go to same line number at right pane",
+        },
+        ["<left>"] = {
+          ":let linenum=getpos('.')[1]|:wincmd h|:call cursor(linenum,0)<cr>",
+          desc = "go to same line number at left pane",
+        },
+        ["<up>"] = {
+          ":let linenum=getpos('.')[1]|:wincmd k|:call cursor(linenum,0)<cr>",
+          desc = "go to same line number at upper pane",
+        },
+        ["<down>"] = {
+          ":let linenum=getpos('.')[1]|:wincmd j|:call cursor(linenum,0)<cr>",
+          desc = "go to same line number at lower pane",
+        },
         ["o"] = { "%", desc = "jump to opening/closing pair" },
         ["T"] = { "zt", desc = "move current line to top" },
         ["E"] = { "$", desc = "move cursor to end of line" },
@@ -82,6 +94,10 @@ return {
         ["<"] = { "<gv", desc = "retain selection after un-indentation" },
         ["gf"] = { "?def <cr>w", desc = "go to start of function" },
         ["gb"] = { "?def <cr>w*", desc = "go to calling function" },
+        ["<A-b>"] = { "gt", desc = "go to next buffer" },
+        ["<A-t>"] = { ":tabn<CR>", desc = "go to next tab" },
+        ["<Leader>Z"] = { ":tab split<CR>", desc = "zoom into current pane" },
+        ["<Leader>z"] = { "mAZZ'A<ESC>T", desc = "un-zoom" },
       },
       t = {
         -- setting a mapping to false will disable it
