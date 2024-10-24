519ac36d841c49f80d70de8ecb4c9b18336bcd71
Noel Quintos <npquintos@gmail.com>
2024-10-24 11:56:02 -0600

    Update user.lua

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -5,7 +5,36 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 ---@type LazySpec
 return {
-
+  {
+      "nvim-neorg/neorg",
+      build = ":Neorg sync-parsers",
+      ft = "norg",
+      LocalLeader = ";",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      event = "VeryLazy",
+      opts = {
+        load = {
+          ["core.defaults"] = {
+          }, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.keybinds"] = {}, -- Adds default keybindings
+          ["core.completion"] = {
+            config = {
+              engine = "nvim-cmp",
+            },
+          }, -- Enables support for completion plugins
+          ["core.journal"] = {}, -- Enables support for the journal module
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+              default_workspace = "notes",
+            },
+          },
+        },
+      },
+  },
   -- == Examples of Adding Plugins ==
 
   "andweeb/presence.nvim",
