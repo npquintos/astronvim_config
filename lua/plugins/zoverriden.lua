40707dc046cdcf0f570031296bdf70c371ff1144
Noel Quintos <npquintos@gmail.com>
2024-12-20 08:32:45 -0700

    Update zoverriden.lua

--- lua/plugins/zoverriden.lua
+++ lua/plugins/zoverriden.lua
@@ -1,5 +1,15 @@
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
+-- This is where you override the default settings of the plugins you installed format is:
+-- {
+--    "<github_folder>/<plugin_name>",
+--    opts = function(_, opts) # This is how you override ALL previous settings
+
+--    opts = {
+--       param1 = value1,  # This is how you add your own BUT NOT guaranteed !!
+--    },
+-- },
+
 -- Customize Treesitter
 
 local telescope = require "telescope"
