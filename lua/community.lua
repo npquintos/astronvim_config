c2bc3773c0b702bbc7fa162b65d20b89f0c67da2
Noel Quintos <npquintos@gmail.com>
2025-03-11 08:55:32 -0600

    Update community.lua

--- lua/community.lua
+++ lua/community.lua
@@ -7,15 +7,15 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
+  -- { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
   { import = "astrocommunity.motion.flash-nvim" },
   { import = "astrocommunity.file-explorer.oil-nvim" },
   { import = "astrocommunity.colorscheme.bamboo-nvim" },
-  { import = "astrocommunity.note-taking.neorg" },
-  { import = "astrocommunity.snippet.nvim-snippets" },
+  -- { import = "astrocommunity.note-taking.neorg" },
+  -- { import = "astrocommunity.snippet.nvim-snippets" },
   { import = "astrocommunity.completion.coq_nvim" },
-  { import = "astrocommunity.editing-support.refactoring-nvim" },
- -- { import = "astrocommunity.editing-support.nvim-origami" },
+  -- { import = "astrocommunity.editing-support.refactoring-nvim" },
+  -- { import = "astrocommunity.editing-support.nvim-origami" },
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
 }
